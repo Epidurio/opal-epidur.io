ae29a99c26bfaf475f771309cc2a5c7bb6a8895f
Marcus Baw <marcusbaw@gmail.com>
2018-02-09 16:41:24 +0000

    added PatientLists for EpiduralRequests and EpiduralFollowups and stub display template

--- epidurio/patient_lists.py
+++ epidurio/patient_lists.py
@@ -3,8 +3,8 @@ Defining Opal PatientLists
 """
 from opal import core
 from opal.models import Episode
-
 from epidurio import models
+
 
 class AllPatientsList(core.patient_lists.PatientList):
     display_name = 'All Patients'
@@ -17,3 +17,27 @@ class AllPatientsList(core.patient_lists.PatientList):
 
     def get_queryset(self, **kwargs):
         return Episode.objects.all()
+
+
+class EpiduralRequestsList(core.patient_lists.TaggedPatientList):
+    display_name = "Epidural Requests"
+    tag = "epidural_requested"
+
+    schema = [
+        models.Allergies,
+        models.Demographics,
+        models.EpiduralRequest,
+    ]
+
+
+class EpiduralFollowUpsList(core.patient_lists.TaggedPatientList):
+    display_name = "Epidural Followups"
+    tag = "epidural_completed"
+    direct_add = False
+    # patients cannot be direct added; they must have had an epidural to be added
+
+    schema = [
+        models.Demographics,
+        models.EpiduralRequest,
+        models.EpiduralInsertion,
+    ]
