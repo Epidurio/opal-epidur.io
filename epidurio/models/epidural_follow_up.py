5eb0948fd99abbc09575ac3b5046570d0074dfd9
timcknowles <tim.c.knowles@gmail.com>
2018-02-25 10:41:13 +0000

    adding to requests forms

--- epidurio/models/epidural_follow_up.py
+++ epidurio/models/epidural_follow_up.py
@@ -8,12 +8,26 @@ class EpiduralFollowUp(models.EpisodeSubrecord):
     time_of_follow_up = fields.DateTimeField(
         blank=True, null=True
     )
+    performed_by = fields.TextField(
+        null=True,
+        blank=True,
+
+    )
     headache = fields.BooleanField(default=False)
     has_not_mobilised = fields.BooleanField(default=False)
     weakness = fields.BooleanField(default=False)
     sensory_disturbance = fields.BooleanField(default=False)
     back_pain = fields.BooleanField(default=False)
     has_not_passed_urine = fields.BooleanField(default=False)
+    itu_admission = fields.BooleanField(default=False)
+    general_anaesthesia = fields.BooleanField(default=False)
+    home_before_follow_up = fields.BooleanField(default=False)
+    post_natal_appointment_required = fields.BooleanField(default=False)
+    other = fields.TextField(
+        null=True,
+
+    )
+
 
 
 class SatisfactionRating(models.EpisodeSubrecord):
